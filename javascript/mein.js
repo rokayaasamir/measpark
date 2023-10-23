dad85baa376164961695a4034834b9e96e7d61d5
rokayaasamir <121617456+rokayaasamir@users.noreply.github.com>
2023-10-24 01:30:58 +0300

    Add files via upload

---
+++ javascript/mein.js
@@ -0,0 +1,42 @@
+/*
+<div class="mobile col-md-4 mb-3 mb-md-0">
+<div class="card h-50% ">
+  <h4>APPS</h4>
+        <h2>ChatGPT’s mobile app hit record $4.58M in revenue last month, but growth is slowing
+        </h2>
+        <hr class="col-mb-4 mb-md-0">
+
+        <h4>APPS</h4>
+        <h2>ChatGPT’s mobile app hit record $4.58M in revenue last month, but growth is slowing
+        </h2>
+        <hr class="col-mb-4 mb-md-0">
+
+        <h4>APPS</h4>
+        <h2>ChatGPT’s mobile app hit record $4.58M in revenue last month, but growth is slowing
+        </h2>
+    
+  
+</div>
+</div>
+
+<div class=" col-md-4 mb-3 mb-md-0">
+<div class="card h-50% ">
+  <div class="card-body text-center">
+    
+      <h4 class=" m-0">Olivia Martinez</h4>
+    
+      <div class=" had small -50">i had a great experience shopping on this  website. The clothes i bought are fashionable  and comfortable. Highly satisfied!</div>
+  </div>
+</div>
+</div>
+<i class="group"><img src="./images/Group 8.png" alt=""><img src="./images/Group 9.png" alt=""></i>  
+</div>
+
+
+
+
+
+
+
+
+
